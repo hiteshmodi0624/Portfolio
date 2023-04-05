ae983d54d12f43eed3eb342b7a1ea539f0516719
Hitesh <hitesh_12014132@nitkkr.ac.in>
2023-04-05 23:21:01 +0530

    added head

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,12 +3,25 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import { Analytics } from '@vercel/analytics/react'
 ;
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
       <Layout>
+          <Head>
+              <meta
+                  name="viewport"
+                  content="width=device-width, initial-scale=1"
+              />
+              <title>Hitesh Modi</title>
+              <meta
+                  name="description"
+                  content="Hello I'm Hitesh Modi.A skilled full-stack developer with expertise in Next.js, Typescript, TailwindCss, React, and Node.js. I love contributing to open-source projects and solving coding challenges."
+              />
+              
+          </Head>
           <Component {...pageProps} />
-          <Analytics/>
+          <Analytics />
       </Layout>
   );
 }
