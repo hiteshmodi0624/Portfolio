662590af4036dde735dc2922c1002f753f66ec5a
Hitesh Modi <mail@hiteshmodi.in>
2024-12-19 13:57:19 +0530

    Update resume.ts

--- data/resume.ts
+++ data/resume.ts
@@ -1,41 +1,71 @@
 const resumeData = {
     experiences: [
         {
+            company: "Digital Alpha Technologies",
+            position: "Software Developer",
+            time: "February 2024 - Present",
+            description: [
+                "Spearheaded the migration of a navigation application from Flutter to React Native, delivering a production ready app in 3 months, resulting in a significant improvement in development efficiency.",
+                "Authored rnmapbox-ditch, a custom library that extends @rnmapbox, that enables Smart ETA Calculations, noseline computation, breadcrumb tracking and other advanced features, increasing app functionality by 25%.",
+                "Integrated AI-driven features using ChatGPT, leveraging prompt engineering to automate user interactions and enhance engagement by 25%.",
+                "Optimized CPU utilization and memory efficiency, reducing resource consumption by 50% while maintaining high performance across mobile and desktop platforms.",
+                "Mentored a junior developer, accelerating their on boarding and contributing to a 20% increase in team productivity."
+            ],
+            current: true,
+            location: "Remote"
+        },
+        {
+            company: "XMB Technology",
+            position: "Full Stack Developer (Part Time)",
+            time: "January 2024 - August 2024",
+            description: [
+                "Applied SOLID principles and best practices in developing 10+ APIs and email templates using TypeScript and React.",
+                "Conducted comprehensive Jest testing across 50+ test cases, ensuring robustness and quality.",
+            ],
+            current: false,
+            location: "Remote"
+        },
+        {
             company: "Umego",
-            position: "Full Stack Developer",
-            time: "December 2023 - Present",
-            description: [`Showcased versatility by quickly acquiring proficiency in emerging technologies such as NextJs, TypeScript, resulting in
-a 25% increase in team efficiency.`,`Achieved a 15% improvement in user satisfaction scores by crafting visually appealing and user-friendly interfaces
-using React.`,`Collaborated cross-functionally at Umego, contributing to reliable, scalable, and high-performance software. Utilized Jira,
-reducing task resolution time by 40% for timely project deliveries.`],
-            current:true,
-            location:"Remote"
+            position: "Full Stack Developer (Part Time)",
+            time: "September 2023 - December 2023",
+            description: [
+                `Achieved a 15% improvement in user engagement scores by creating visually appealing and user-friendly interfaces.`,
+                `Increased Admin panel efficiency by 20% with features like filtering while image uploading.`,
+                `Designed new interfaces and backend components using Next.js 13, Node.js, and TypeScript.`
+            ],
+            current: false,
+            location: "Remote"
         },
         {
             company: "MindGinie",
             position: "Full Stack Developer (Intern)",
             time: "June 2023 - August 2023",
-            description: [`Researched on 5+ fitness app APIs for seamless integration with the product.`,`Crafted 3 engaging games leveraging React Native and CSS design.`,`Engineered a data retrieval system, which reduced run time by 90%.`,
-`Conducted comprehensive code reviews, leading to a 30% decrease in post-deployment issues and enhancing overall code quality.`],
-            current:false,
-            location:"Remote"
+            description: [
+                `Engineered 3 new engaging games utilising React Native and CSS design principles.`,
+                `Enhanced performance by caching, reducing initial data retrieval time by 90%.`
+            ],
+            current: false,
+            location: "Remote"
         },
         {
             company: "Nathead",
             position: "Founder & Full Stack Developer",
             time: "July 2021 - June 2023",
-            description: [`Designed and maintained 15+ websites using HTML, CSS and JavaScript.`,
-            `Mentored and guided 5 new team members in website development and technical discussions.`],
-            current:false,
-            location:"Remote"
+            description: [
+                `Created and maintained 15+ websites using HTML, CSS and JavaScript.`,
+                `Integrated SDLC with project managers for the gathering of requirements, leading to an increase in the customer satisfaction score by 20%.`
+            ],
+            current: false,
+            location: "Remote"
         },
         {
             company: "Chegg India",
             position: "Math Expert (Freelancer)",
             time: "2021 - 2023",
             description: [`Assisted students clarify their math doubts by solving the sums in the topics that include Calculus, Geometry, and Algebra.`],
-            current:false,
-            location:"Remote"
+            current: false,
+            location: "Remote"
         },
     ],
     education: [
@@ -43,9 +73,9 @@ reducing task resolution time by 40% for timely project deliveries.`],
             company: "National Institute of Technology, Kurukshetra",
             position: "BTech",
             time: "2020 - 2024",
-            description: [`Current CGPA:8.48`,'Member of Embedded-System and Robotics club.'],
-            current:+(new Date().getFullYear())<=2024,
-            location:"Kurukshetra"
+            description: [`Current CGPA:8.21`, 'Member of Embedded-System and Robotics club.'],
+            current: false,
+            location: "Kurukshetra"
         },
         {
             company: "Academind",
@@ -58,16 +88,16 @@ reducing task resolution time by 40% for timely project deliveries.`],
                 'Next.js & React - The Complete Guide (incl. Two Paths!)',
                 'Understanding TypeScript'
             ],
-            current:false,
-            location:"Online"
+            current: false,
+            location: "Online"
         },
         {
             company: "Maria's Public School",
             position: "Higher Secondary Education",
             time: "2017-2019",
             description: [],
-            current:false,
-            location:"Guwahati"
+            current: false,
+            location: "Guwahati"
         },
     ],
 };
