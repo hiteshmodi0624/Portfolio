b425741e3d7dcd9eba7eddd6f60b6de00453bb68
Hitesh Modi <mail@hiteshmodi.in>
2024-12-19 14:08:47 +0530

    fix: hobbies, about

--- components/sections/about/hobbies.tsx
+++ components/sections/about/hobbies.tsx
@@ -1,23 +1,17 @@
 import IconWrapper from "@/components/ui/icon-wrapper"
-import { BiCricketBall, BiMoviePlay, BiMusic } from "react-icons/bi"
-import { FaChessKing, FaFootballBall, FaLaptopCode, FaAppStore} from "react-icons/fa"
+import { FaChessKing, FaLaptopCode } from "react-icons/fa";
 
 const Hobbies=()=>{
     return (
-        <div className="p-1 items-center">
-            <h3 className="uppercase bg-tertiary2 dark:bg-tertiary1 px-2 dark:text-secondary1 text-primary2 font-bold inline py-1 min-w-max">
-                Loves to:
-            </h3>
-            <div className="grid text-center grid-cols-[repeat(auto-fit,8rem)] justify-center items-start">
-                <IconWrapper icon={<FaChessKing />} title="Play Chess" />
-                <IconWrapper icon={<FaLaptopCode />} title="Code" />
-                <IconWrapper
-                    icon={<BiMoviePlay />}
-                    title="Watch TV Series"
-                />
-                <IconWrapper icon={<FaFootballBall />} title="Play Football" />
-            </div>
+      <div className="p-1 items-center">
+        <h3 className="uppercase bg-tertiary2 dark:bg-tertiary1 px-2 dark:text-secondary1 text-primary2 font-bold inline py-1 min-w-max">
+          Loves to:
+        </h3>
+        <div className="grid text-center grid-cols-[repeat(auto-fit,8rem)] justify-center items-start">
+          <IconWrapper icon={<FaLaptopCode />} title="Code" />
+          <IconWrapper icon={<FaChessKing />} title="Play Chess" />
         </div>
+      </div>
     );
 }
 export default Hobbies
--- data/about.ts
+++ data/about.ts
@@ -4,20 +4,18 @@ function calculate_age(dob:Date) {
 }
 
 const aboutData = {
-    title: "Hello I'm Hitesh Modi.",
-    article: [
-        `A skilled full-stack developer with expertise in Next.js, Typescript, TailwindCss, React, and Node.js. 
-        I love contributing to open-source projects and solving coding challenges. 
-        Let's connect via social media or send me a message on this website to learn more!`,
-    ],
-    details: [
-        { key:"Age", value:calculate_age(new Date(2001,6,24)) },
-        { key:"Resides in", value:"Guwahati, India"},
-        { key:"Email" , value:"mail@hiteshmodi.in" },
-    ],
-    profiles:{
-        leetcode:'https://leetcode.com/hiteshmodi0624/',
-        gfg:'https://auth.geeksforgeeks.org/user/hiteshmodi0624',
-    }
+  title: "Hello I'm Hitesh Modi.",
+  article: [
+    `A Software Developer with experience in both front-end and back-end development. I thrive on problem-solving and developing innovative solutions using the latest technologies. I have a strong foundation in React, React Native, Node.js, and AWS technologies, with a special interest in AI integrations and map-based applications. Feel free to reach out to me so that we can work on a project together!`,
+  ],
+  details: [
+    { key: "Age", value: calculate_age(new Date(2001, 6, 24)) },
+    { key: "Resides in", value: "Guwahati, India" },
+    { key: "Email", value: "mail@hiteshmodi.in" },
+  ],
+  profiles: {
+    leetcode: "https://leetcode.com/hiteshmodi0624/",
+    gfg: "https://auth.geeksforgeeks.org/user/hiteshmodi0624",
+  },
 };
 export default aboutData
